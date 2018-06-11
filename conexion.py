d52e6a9c1b3e01903e393f9d41b6a4d1d1109064
Odin2 <alepiojo05@gmail.com>
2018-06-11 00:12:39 -0600

    cleanList

--- conexion.py
+++ conexion.py
@@ -25,6 +25,30 @@ import xml.etree.ElementTree as ET
 path= "E:\Desktop\proyecto3_mongo\\reuters21578"
 archivo=os.path.join(path,"test.xml")
 
+'''
+##cleanList: ajusta los valores multiples de los documentos REUTERS
+##input i: Documento REUTERS
+##input Key: Nombre del campo a ajustar
+ej.
+ "PLACES": {
+                "D": [
+                    "usa",
+                    "ussr"
+                ]
+            },
+ -->
+  "PLACES": [
+                "usa",
+                "ussr"
+            ],
+'''
+def cleanList(i,Key):
+    if i[Key] is not None:
+        if 'D' in i[Key]:
+            temp = i[Key]['D']
+            del i[Key]['D']
+            i.update({Key:temp})
+    
 with open(archivo,encoding='utf-8', errors='ignore') as f:
     xmlreader=f.read()
     doc=xmltodict.parse(xmlreader)
@@ -34,6 +58,13 @@ with open(archivo,encoding='utf-8', errors='ignore') as f:
             del i['MKNOTE']
         if 'UNKNOWN' in i:
             del i['UNKNOWN']
+        cleanList(i,'PLACES')
+        cleanList(i,'TOPICS')
+        cleanList(i,'PEOPLE')
+        cleanList(i,'ORGS')
+        cleanList(i,'EXCHANGES')
+        
+
     jsonString = json.dumps(doc, indent=4)
     print(jsonString)
 
